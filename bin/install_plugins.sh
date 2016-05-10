78b2e009b55147ba9af244c761fac2b125e4d161
Neil Mahoney <neiljosephmahoney@gmail.com>
2016-05-10 15:56:20 -0700

    Initial vim plugin installation

---
+++ bin/install_plugins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+root="$HOME/.vim/"
+remote="https://github.com/"
+ext=".git"
+bundle=$root"bundle/"
+autoload=$root"autoload/"
+
+plugins=(
+    "tpope/vim-surround"
+    "tpope/vim-unimpaired"
+    "tpope/vim-commentary"
+    "tpope/vim-fugitive"
+    "roman/golden-ratio"
+    "wincent/command-t"
+    "altercation/vim-colors-solarized")
+
+
+if [[ -d "$autoload" ]]
+then
+  cd $autoload
+  curl -LSso pathogen.vim https://tpo.pe/pathogen.vim
+else
+  echo $autoload "does not exist"
+fi
+
+if [[ -d "$bundle" ]]
+then
+  cd $bundle
+
+  for plugin in "${plugins[@]}"
+  do
+    git clone $remote$plugin$ext
+  done
+else
+  echo $bundle "does not exist"
+fi

5f83bab46dcf4e160e7f3b56f3fa70ad9b66a309
Neil Mahoney <neil@planetargon.com>
2016-12-18 00:26:35 -0800

    Linking tmux

--- bin/link_dotfiles.sh
+++ bin/link_dotfiles.sh
@@ -1,7 +1,9 @@
 ln -fs $HOME/.dotfiles/.gitconfig $HOME
 ln -fs $HOME/.dotfiles/.gitignore $HOME
-ln -fs $HOME/.dotfiles/.tmux.conf $HOME
 ln -fs $HOME/.dotfiles/.Rprofile $HOME
 ln -fs $HOME/.dotfiles/bin $HOME
+
+ln -fs $HOME/dev/open-source/maximum-awesome/tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/dev/open-source/maximum-awesome/tmux.conf.local $HOME/.tmux.conf.local
 
 ln -fs $HOME/.dotfiles/.*rc $HOME

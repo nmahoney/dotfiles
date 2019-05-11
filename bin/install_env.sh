1be2136d73dc274ec5b7e20965a5f6fb72f9aec6
Neil Mahoney <neiljosephmahoney@gmail.com>
2019-05-11 13:41:36 -0400

    replace tmux config

--- bin/install_env.sh
+++ bin/install_env.sh
@@ -1,9 +1,9 @@
 mkdir ~/dev
 mkdir ~/dev/open-source
 
-git clone git@github.com:square/maximum-awesome.git ~/dev/open-source/maximum-awesome
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/gpakosz/.tmux.git ~/dev/open-source/.tmux
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- bin/link_dotfiles.sh
+++ bin/link_dotfiles.sh
@@ -2,8 +2,8 @@ ln -fs $HOME/.dotfiles/.gitconfig $HOME
 ln -fs $HOME/.dotfiles/.Rprofile $HOME
 ln -fs $HOME/.dotfiles/bin $HOME
 
-ln -fs $HOME/dev/open-source/maximum-awesome/tmux.conf $HOME/.tmux.conf
-ln -fs $HOME/dev/open-source/maximum-awesome/tmux.conf.local $HOME/.tmux.conf.local
+ln -fs $HOME/dev/open-source/.tmux/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/dev/open-source/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
 
 ln -fs $HOME/.dotfiles/.*rc $HOME
 ln -fs $HOME/.dotfiles/.*ignore $HOME

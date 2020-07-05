37721658131f0db87a3e9e575aff916aec64133c
Neil Mahoney <neiljosephmahoney@gmail.com>
2020-07-05 15:48:36 -0400

    end-to-end mac installation

--- bin/install_env.sh
+++ bin/install_env.sh
@@ -1,8 +1,17 @@
 if [[ $(uname) =~ 'Darwin' ]]; then
+  if ! which brew 1>/dev/null; then
+    echo 'Installing homebrew...'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+
+  echo 'Installing packages...'
+  sh $HOME/.dotfiles/bin/mac/install_packages.sh
+
   echo 'Installing mac defaults...'
   sh $HOME/.dotfiles/bin/mac/set_defaults.sh
 fi
 
+echo 'Installing open source tools...'
 mkdir ~/dev
 mkdir ~/dev/open-source
 
@@ -13,14 +22,16 @@ git clone https://github.com/gpakosz/.tmux.git ~/dev/open-source/.tmux
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo 'Linking dotfiles...'
 sh $HOME/.dotfiles/bin/link_dotfiles.sh
 
+echo 'Setting shell...'
 custom_shell="/usr/local/bin/zsh"
 if ! grep -q $custom_shell /etc/shells; then
-  echo "Adding custom shell to /etc/shells..."
   echo $custom_shell | sudo tee -a /etc/shells > /dev/null
 fi
 
 [ $SHELL != $custom_shell ] && chsh -s $custom_shell
 
+echo 'Setting keys...'
 [ ! -f $HOME/.ssh/id_rsa ] && ssh-keygen -t rsa

175e3e7a6045b387d7502ee2a8770d54b07692f7
Neil Mahoney <neiljosephmahoney@gmail.com>
2021-08-27 01:09:56 -0400

    install vim plugins

--- bin/install_env.sh
+++ bin/install_env.sh
@@ -41,6 +41,8 @@ fi
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+vim -c 'PlugInstall' -c 'qa'
+
 npm install -g json-server typescript ts-node yarn
 
 echo 'Linking dotfiles...'
